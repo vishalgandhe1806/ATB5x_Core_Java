42b375e9626bd79b41425dda4e9d7b2263dc5242
vishalgandhe1806 <vishalgandhe1806@gmail.com>
2023-12-30 19:21:25 +0530

    Scond commit

--- src/Dec17/LAB01.java
+++ src/Dec17/LAB01.java
@@ -1,2 +1,11 @@
-package src.Dec17;public class LAB01 {
+package src.Dec17;
+
+public class LAB01
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("New Git Commit");
+
+    }
 }
---
+++ src/Dec18/LAB002.java
@@ -0,0 +1,8 @@
+package src.Dec18;
+
+public class LAB002
+{
+    public static void main(String[] args) {
+        System.out.println("Git commit second ");
+    }
+}
