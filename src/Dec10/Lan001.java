08a421a30b215ca44bfe8d7970a8c07564850b87
vishalgandhe1806 <vishalgandhe1806@gmail.com>
2024-01-04 23:22:30 +0530

    10 Dec

---
+++ src/Dec10/Lan001.java
@@ -0,0 +1,25 @@
+package src.Dec10;
+
+public class Lan001
+{
+    public static void main(String[] args)
+    {
+        // Documentation - Author - Pramod
+        // What you are doing in this code? - I am printing Hello world!!
+        // Expain the code
+
+        System.out.println("Hello World");
+        // System.out.println("Dont print me");
+        // This line will not execute  Single commnet
+
+        // Documentation
+        /**
+         *
+         * Multi Comment
+         * Author  - Pramod
+         * Github Link - https://sdet.live/git
+         *
+         * */
+
+    }
+}
